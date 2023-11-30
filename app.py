2e663aae2e30df15600981064ae6781a19b7702b
maxwelljmacd <81054588+maxwelljmacd@users.noreply.github.com>
2023-11-30 14:04:19 -0500

    Added some buttons for accessing basic database features

--- app.py
+++ app.py
@@ -21,24 +21,36 @@ def login():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-  if not session.get('host') or not session.get('user') or not session.get('password') or not session.get('database'):
-      return redirect(url_for('login'))
-  else:
-      mydb = mysql.connector.connect(
-          host=session['host'],
-          user=session['user'],
-          password=session['password'],
-          database=session['database']
-      )
-      if request.method == 'POST':
-          query = request.form['query']
-          cursor = mydb.cursor()
-          cursor.execute(query)
-          result = cursor.fetchall()
-          column_names = [desc[0] for desc in cursor.description]
-          return render_template('index.html', result=result, column_names=column_names)
-      else:
-          return render_template('index.html')
+    if not session.get('host') or not session.get('user') or not session.get('password') or not session.get('database'):
+        return redirect(url_for('login'))
+    else:
+        mydb = mysql.connector.connect(
+            host=session['host'],
+            user=session['user'],
+            password=session['password'],
+            database=session['database']
+        )
+        if request.method == 'POST':
+            if 'Appointments' in request.form:
+                query = "SELECT CONCAT(p.First_name, ' ', p.Last_name) as Patient_name, a.Assessment_Name, CONCAT(p2.First_name, ' ', p2.Last_name) as Staff_name, ap.Total_cost FROM appointment as ap, assessment as a, person as p, person as p2 WHERE ap.AssessmentID = a.AssessmentID and p.PersonID = ap.PatientID and p2.PersonID = ap.StaffID and ap.status = 'Not Complete'"
+            elif 'Medication Stock' in request.form:
+                query = "select Medication_name, Stock, Unit, Cost_per_unit from medication order by Stock"
+            elif 'Staff Qualifications' in request.form:
+                query = "select q.Qualification_Name, CONCAT(p.First_name, ' ', p.Last_name) as Staff_name, q.Expiry_date from qualification as q, person as p where q.StaffID = p.PersonID"
+            else:
+                query = request.form['query']
+
+            cursor = mydb.cursor()
+            try:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                column_names = [desc[0] for desc in cursor.description]
+                return render_template('index.html', result=result, column_names=column_names)
+            except:
+                print("No query inserted!")
+                return render_template('index.html')
+        else:
+            return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
